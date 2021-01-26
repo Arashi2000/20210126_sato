6038937e743e36ba5c65f409d0fa1dc25f6c69b3
Arashi2000 <76526188+Arashi2000@users.noreply.github.com>
2021-01-26 23:45:51 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,147 @@
+<template>
+<div id="app">
+<div class="home">
+<input type="text" v-model="text"/>
+<button class="button-side" @click="sendAdress">住所自動入力</button>
+</div>
+<div class="text-area">
+<p class="address">
+    address:</p>
+<p class="show-text">{{text}}</p>
+</div>
+</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text:""
+    };
+  },
+  methods: {
+    async getAdress() {
+      this.adress = await this.$axios.$get('https://postcode-jp.com/')
+    },
+    sendAdress() {
+      console.log("Hello");
+    }
+  }
+};
+</script>
+
+<style>
+html, body, div, span, object, iframe,
+h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+abbr, address, cite, code,
+del, dfn, em, img, ins, kbd, q, samp,
+small, strong, sub, sup, var,
+b, i,
+dl, dt, dd, ol, ul, li,
+fieldset, form, label, legend,
+table, caption, tbody, tfoot, thead, tr, th, td,
+article, aside, canvas, details, figcaption, figure,
+footer, header, hgroup, menu, nav, section, summary,
+time, mark, audio, video {
+    margin:0;
+    padding:0;
+    border:0;
+    outline:0;
+    font-size:100%;
+    vertical-align:baseline;
+    background:transparent;
+}
+
+body {
+    line-height:1;
+}
+
+article,aside,details,figcaption,figure,
+footer,header,hgroup,menu,nav,section {
+    display:block;
+}
+
+nav ul {
+    list-style:none;
+}
+
+blockquote, q {
+    quotes:none;
+}
+
+blockquote:before, blockquote:after,
+q:before, q:after {
+    content:'';
+    content:none;
+}
+
+a {
+    margin:0;
+    padding:0;
+    font-size:100%;
+    vertical-align:baseline;
+    background:transparent;
+}
+
+/* change colours to suit your needs */
+ins {
+    background-color:#ff9;
+    color:#000;
+    text-decoration:none;
+}
+
+/* change colours to suit your needs */
+mark {
+    background-color:#ff9;
+    color:#000;
+    font-style:italic;
+    font-weight:bold;
+}
+
+del {
+    text-decoration: line-through;
+}
+
+abbr[title], dfn[title] {
+    border-bottom:1px dotted;
+    cursor:help;
+}
+
+table {
+    border-collapse:collapse;
+    border-spacing:0;
+}
+
+/* change border colour to suit your needs */
+hr {
+    display:block;
+    height:1px;
+    border:0;  
+    border-top:1px solid #cccccc;
+    margin:1em 0;
+    padding:0;
+}
+
+input, select {
+    vertical-align:middle;
+}
+
+.home {
+  display: flex;
+  margin: 10px;
+}
+
+.button-side {
+  margin-left: 10px;
+}
+
+.text-area  {
+  margin: 30px 10px;
+  display: flex;
+}
+
+.show-text {
+    margin-left: 10px;
+}
+
+</style>
